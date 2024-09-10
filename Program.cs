191189590d8df1a6f5fd3722828384d1b3863e80
Micah Ma <701004018@GoTVDSB.ca>
2024-09-10 12:00:24 -0400

    fix the cursor location

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@
 
             dice1.DrawRoll(3, 2);
             dice2.DrawRoll(12, 2);
-            Console.WriteLine("Do you think the sum of 2 dices will be higher than 7 or lower?(\'h\' or \'l\')");
+            Console.WriteLine("Do you think the sum of 2 dices will be higher than 7 or lower?(\'h\' or \'l\' or \'7\')");
             bool done = false;
             while (!done)
             {
@@ -20,12 +20,45 @@
                 if (input == "h"||input == "higher")
                 {
                     done = true;
+                    Console.WriteLine("How much do you want to bet? (at least $1.00)");
+                    double bet;
+                    while (!double.TryParse(Console.ReadLine(),out bet)||bet <= 1|| bet > balance)
+                    {
+                        Console.WriteLine("Please enter a number");
+                    }
+                    balance = balance-bet;  
+                    UpdateBank();
+
+                    dice1.RollDie();
+                    dice1.DrawRoll(3, 2);
+                    !
+                    Console.WriteLine("Do you want to add on to your bet?(ENTER to skip)");
+                    if (Console.ReadLine() != null)
+                    {
+                        double add;
+                        while (!double.TryParse(Console.ReadLine(), out add) || bet <= 1 || bet > balance)
+                        {
+                            Console.WriteLine("Please enter a number");
+                        }
+                        balance = balance - add;
+                        bet = bet + add;
+                        UpdateBank();
+                    }
+
+
+                    dice2.RollDie();
+                    dice2.DrawRoll(12, 2);
+                    if (dice1.Roll+dice2.Roll > 7)
+                    {
+
+                    }
+                    
                 }
                 else if (input == "l" || input == "lower")
                 {
                     done = true;
                 }
-                else if (input == "q" || input == "quit")
+                else if (input == "7")
                 {
                     done = true;
                 }
