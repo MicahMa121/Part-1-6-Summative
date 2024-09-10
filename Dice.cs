dcddffe81593cf98878e3189cf56a5c67903ecbb
Micah Ma <701004018@GoTVDSB.ca>
2024-09-10 09:42:15 -0400

    Add project files.

---
+++ Dice.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1_6_Summative
+{
+    public class Dice
+    {
+        private int _roll;
+        private Random _generator;
+
+        public Dice()
+        {
+            _generator = new Random();
+            _roll = 0;
+        }
+
+
+        public int Roll {  get { return _roll; } set { _roll = value; } }
+
+        public int RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+            return _roll;
+        }
+
+        public void DrawRoll(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            if (_roll == 1)
+            {
+                Console.WriteLine("-----");
+                Console.SetCursorPosition(x, y+1);
+                Console.WriteLine("|   |");
+                Console.SetCursorPosition(x, y + 2);
+                Console.WriteLine("| o |");
+                Console.SetCursorPosition(x, y + 3);
+                Console.WriteLine("|   |");
+                Console.SetCursorPosition(x, y + 4);
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 2)
+            {
+                Console.WriteLine("-----");
+                Console.SetCursorPosition(x, y + 1);
+                Console.WriteLine("|o  |");
+                Console.SetCursorPosition(x, y + 2);
+                Console.WriteLine("|   |");
+                Console.SetCursorPosition(x, y + 3);
+                Console.WriteLine("|  o|");
+                Console.SetCursorPosition(x, y + 4);
+                Console.WriteLine("-----");
+            }
+
+            else if (_roll == 3)
+            {
+                Console.WriteLine("-----");
+                Console.SetCursorPosition(x, y + 1);
+                Console.WriteLine("|o  |");
+                Console.SetCursorPosition(x, y + 2);
+                Console.WriteLine("| o |");
+                Console.SetCursorPosition(x, y + 3);
+                Console.WriteLine("|  o|");
+                Console.SetCursorPosition(x, y + 4);
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 4)
+            {
+                Console.WriteLine("-----");
+                Console.SetCursorPosition(x, y + 1);
+                Console.WriteLine("|o o|");
+                Console.SetCursorPosition(x, y + 2);
+                Console.WriteLine("|   |");
+                Console.SetCursorPosition(x, y + 3);
+                Console.WriteLine("|o o|");
+                Console.SetCursorPosition(x, y + 4);
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 5)
+            {
+                Console.WriteLine("-----");
+                Console.SetCursorPosition(x, y + 1);
+                Console.WriteLine("|o o|");
+                Console.SetCursorPosition(x, y + 2);
+                Console.WriteLine("| o |");
+                Console.SetCursorPosition(x, y + 3);
+                Console.WriteLine("|o o|");
+                Console.SetCursorPosition(x, y + 4);
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 6)
+            {
+                Console.WriteLine("-----");
+                Console.SetCursorPosition(x, y + 1);
+                Console.WriteLine("|o o|");
+                Console.SetCursorPosition(x, y + 2);
+                Console.WriteLine("|o o|");
+                Console.SetCursorPosition(x, y + 3);
+                Console.WriteLine("|o o|");
+                Console.SetCursorPosition(x, y + 4);
+                Console.WriteLine("-----");
+            }
+            else
+            {
+                Console.WriteLine("-----");
+                Console.SetCursorPosition(x, y + 1);
+                Console.WriteLine("|   |");
+                Console.SetCursorPosition(x, y + 2);
+                Console.WriteLine("| ? |");
+                Console.SetCursorPosition(x, y + 3);
+                Console.WriteLine("|   |");
+                Console.SetCursorPosition(x, y + 4);
+                Console.WriteLine("-----");
+            }
+        }
+
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Part_1_6_Summative
+{
+    internal class Program
+    {
+        static double balance;
+        static void Main(string[] args)
+        {
+            balance = 100;
+            UpdateBank();
+            Dice dice1 = new Dice();
+            Dice dice2 = new Dice();
+
+            dice1.DrawRoll(3, 2);
+            dice2.DrawRoll(12, 2);
+            Console.WriteLine("Do you think the sum of 2 dices will be higher than 7 or lower?(\'h\' or \'l\')");
+            bool done = false;
+            while (!done)
+            {
+                string input = Console.ReadLine();
+                if (input == "h"||input == "higher")
+                {
+                    done = true;
+                }
+                else if (input == "l" || input == "lower")
+                {
+                    done = true;
+                }
+                else if (input == "q" || input == "quit")
+                {
+                    done = true;
+                }
+            }
+        }
+        public static void UpdateBank()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("Hello, You have " + balance.ToString("C")+" in your bank!");
+        }
+    }
+}
